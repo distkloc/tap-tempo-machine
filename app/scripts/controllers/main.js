1c1efd07a7a66119a6a60de0c1fded56b578f8cb
distkloc <rectpali+ws@gmail.com>
2015-02-15 21:00:37 +0900

    Use ngClick for ngTouch instead of clickCollector to improve smart phone click performance

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,9 +8,20 @@
  * Controller of the tapTempoMachineApp
  */
 angular.module('tapTempoMachineApp')
-  .controller('MainCtrl', ['$scope', 'tapTempo', function ($scope, tapTempo) {
+  .controller('MainCtrl', ['$scope', 'tapTempo', '$timeout', function ($scope, tapTempo, $timeout) {
 
     $scope.bpm = 0;
+
+    var clickSource = $scope.$createObservableFunction('collectClick');
+    tapTempo.add(clickSource);
+
+    clickSource.subscribe(function () {
+      $scope.clicked = 'clicked';
+
+      $timeout(function () {
+        $scope.clicked = '';
+      }, 50);
+    });
 
     $scope.$on('tapTempo.update', function() {
       tapTempo.bpmSource()
