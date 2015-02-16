a8497808a6d35bcf1e5e1c75028b14a069161b46
distkloc <rectpali+ws@gmail.com>
2015-02-17 02:50:43 +0900

    Add lightning directive to flash element

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,20 +8,11 @@
  * Controller of the tapTempoMachineApp
  */
 angular.module('tapTempoMachineApp')
-  .controller('MainCtrl', ['$scope', 'tapTempo', '$timeout', function ($scope, tapTempo, $timeout) {
+  .controller('MainCtrl', ['$scope', 'tapTempo', function ($scope, tapTempo) {
 
     $scope.bpm = 0;
 
-    var clickSource = $scope.$createObservableFunction('collectClick');
-    tapTempo.add(clickSource);
-
-    clickSource.subscribe(function () {
-      $scope.clicked = 'clicked';
-
-      $timeout(function () {
-        $scope.clicked = '';
-      }, 50);
-    });
+    tapTempo.add('click', $scope.$createObservableFunction('collectClick'));
 
     $scope.$on('tapTempo.update', function() {
       tapTempo.bpmSource()
